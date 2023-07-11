0402ef232ba56e8fe7ad6fd550c2271b829e574b
GlizzyProductions <131416260+GlizzyProductions@users.noreply.github.com>
2023-07-11 11:27:41 +0000

    cleared paladin error
    by copying the solution for dwarf.h and swapping the constructor names

--- Paladin.h
+++ Paladin.h
@@ -1,7 +1,20 @@
 #pragma once
+#include "Character.h"
+#include "Utility.h"
 
 struct Paladin : Character
 {
+
+    Paladin (std::string name_, int hp, int armor_, int attackDamage_ = 10 ) : 
+    Character( hp, armor_, attackDamage_ ),
+    name (name_)
+    {
+        
+    }
+    
+    const std::string& getName() override { return name; }
+    std::string getStats() override { return getCharacterStats(this); }
+
 private:
     const std::string name;
 };
