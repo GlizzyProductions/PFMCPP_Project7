d55e647fee630e1832d6a78628378d92b5fa8106
GlizzyProductions <131416260+GlizzyProductions@users.noreply.github.com>
2023-07-11 11:13:42 +0000

    processing Dwarf.h
    included utility.h;
    added virtual functions getName & getStats;
    returned "name" & "getCharacterStats";
    changed pointer to a character to point to Dwarf;

--- Dwarf.h
+++ Dwarf.h
@@ -1,7 +1,15 @@
 #pragma once
 #include "Character.h"
+#include "Utility.h"
+
 struct Dwarf : Character
 {
+
+Dwarf (int hp, int armor_, int attackDamage_ ) : Character( hp, armor, attackDamage ){}
+
+const std::string& getName() override { return name }
+std::string getStats() override { return getCharacterStats(this) }
+             
 private:
     const std::string name;
 };
