f0f5f429ed0b080e05c07526068ec183470e4bb1
GlizzyProductions <131416260+GlizzyProductions@users.noreply.github.com>
2023-07-11 11:22:54 +0000

    cleared dwarf error

--- Dwarf.h
+++ Dwarf.h
@@ -5,11 +5,16 @@
 struct Dwarf : Character
 {
 
-Dwarf (int hp, int armor_, int attackDamage_ ) : Character( hp, armor_, attackDamage_ ){}
-
-const std::string& getName() override { return name; }
-std::string getStats() override { return getCharacterStats(this); }
-             
-private:
-    const std::string name;
+    Dwarf (std::string name_, int hp, int armor_, int attackDamage_ = 4 ) : 
+    Character( hp, armor_, attackDamage_ ),
+    name (name_)
+    {
+        
+    }
+    
+    const std::string& getName() override { return name; }
+    std::string getStats() override { return getCharacterStats(this); }
+                 
+    private:
+        const std::string name;
 };
